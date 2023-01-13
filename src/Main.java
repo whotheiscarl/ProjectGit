2e49ca8fd28cdee20ebb5206d2f28882548d0031
carl.wenck <wenck.c@stud.dhbw-ravensburg.de>
2023-01-13 13:20:18 +0100

    Übersetzungen auf Englisch

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ public class Main {
         out.println("Welcome to your local ATM.");
         out.println("Please select an option: " + "available:" + "\n" + "1 - Balance" + "\n" +
                 "2 - Withdraw money" + "\n" + "3 - Deposit money" + "\n" + "4 - Get Balance in other currency" + "\n" +
-                "5 - Get new card" + "\n" +  "6 - Block card");
+                "5 - Get new card" + "\n" + "6 - Block card");
 
         int selection = scanner.nextInt();
 
-        switch(selection){
+        switch (selection) {
             case 1:
                 // get balance
                 break;
@@ -27,19 +27,60 @@ public class Main {
                 break;
             case 4:
                 // get Balance on other Currency
+                double exchangeJPY = 139.15; String JPY = "JPY";
+                double exchangeUSD = 1.08; String USD = "USD";
+                double exchangeGBP = 0.89; String GBP = "GBP";
+                double exchangeAUD = 1.55; String AUD = "AUD";
+                double exchangeCAD = 1.45; String CAD = "CAD";
+                int currrencySelection;
+                double newBalance = 0;
+
+                out.print("Please select the currency you want to convert in: ");
+                currrencySelection = scanner.nextInt();
+
+                switch (currrencySelection) {
+
+                    case 1:
+                        newBalance = balance * exchangeAUD;
+                        out.println("Your Balance in " + AUD + " is " + newBalance + " Australian Dollar.");
+                        break;
+                    case 2:
+                        newBalance = balance * exchangeCAD;
+                        out.println("Your Balance in " + CAD + " is " + newBalance + " Canadian Dollar.");
+                        break;
+                    case 3:
+                        newBalance = balance * exchangeUSD;
+                        out.println("Your Balance in " + USD + " is " + newBalance + " US Dollar.");
+                        break;
+                    case 4:
+                        newBalance = balance * exchangeJPY;
+                        out.println("Your Balance in " + JPY + " is " + newBalance + " japanese Yen.");
+                        break;
+                    case 5:
+                        newBalance = balance * exchangeGBP;
+                        out.println("Your Balance in " + GBP + " is " + newBalance + " UK Pounds.");
+                        break;
+                    default: {
+                        out.println("Error: Unknown currency.");
+                    }
+                }
+
                 break;
             case 5: // get new Card
                 break;
             case 6: // Block card
                 break;
             default: {
-                System.out.println("Wrong data was entered");
+                out.println("Wrong data was entered");
             }
+
+            out.println("");
+            out.println("Thank you for your visit.");
+
+            scanner.close();
 
         }
 
     }
-
-
 
 }

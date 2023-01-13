cb6048cb4bd2e5fb77978cedf53f90e873a1fb20
carl.wenck <wenck.c@stud.dhbw-ravensburg.de>
2023-01-13 10:26:11 +0100

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Cihad!");
+
+        out.println("Hello Cihad!");
+        testPush("Alex");
     }
+
+    public static void testPush(String name) {
+        out.println("Hello " + name);
+    }
+
 }
